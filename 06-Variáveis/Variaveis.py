1f4076dcaa03182c43d0f897ec8d15744f1669e4
LOPES Anderson <andlopes@europe.corp.altran.com>
2019-06-29 10:49:40 +0100

    Variáveis.

---
+++ 06-Variáveis/Variaveis.py
@@ -0,0 +1,15 @@
+# Define encoding
+# -*- coding: utf-8 -*-
+print("Olá variáveis!")
+varMsg = "Olá variáveis!"
+print(varMsg)
+
+var1 = 1 # Inteiro
+var2 = 1.5 # Float
+var3 = "I am a String" #Tipo String
+var4 = False
+
+print(var1)
+print(var2)
+print(var3)
+print(var4)
