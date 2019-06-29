1d7af4b12081fef08dda86bc84935830ead11b4e
LOPES Anderson <andlopes@europe.corp.altran.com>
2019-06-29 11:31:41 +0100

    Interador for.

--- prática/Interadores.py
+++ prática/Interadores.py
@@ -1,8 +1,34 @@
 # Define encoding
 # -*- coding: utf-8 -*-
 
+
+# Laço while
 x = 0;
 
+print("==================================")
+print("Laço WHILE")
+print("==================================")
 while x < 10:
     print(x)
     x += 4
+
+#Laço for
+
+lista = [0,1,2,3,4,5]
+lista2 = ["Olá", "Python", "!"]
+lista3 = [0, "Lista", 3.5, True]
+print("==================================")
+print("Laço FOR")
+print("==================================")
+print("Laço FOR")
+print("---------- Lista 1 ----------")
+for i in lista:
+    print(i)
+
+print("---------- Lista 2 ----------")
+for i in lista2:
+    print(i)
+
+print("- Lista com tipos diferentes -")
+for i in lista3:
+    print(i)
