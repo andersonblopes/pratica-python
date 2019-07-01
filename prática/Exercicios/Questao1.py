b3c1928c5cfccd02fd3fac41ff91db66df1030f6
LOPES Anderson <andlopes@europe.corp.altran.com>
2019-07-01 21:09:52 +0100

    Exercicio 2.

--- prática/Exercicios/Questao1.py
+++ prática/Exercicios/Questao1.py
@@ -1,7 +1,9 @@
 # 1 - Faça um programa que receba a idade do usuário e diga se ele é maior ou menor de idade.  
 idade = int(input("Digite sua idade:"))
 
-if idade < 18:
-    print("Você é de menor!")
-else:
+if idade < 0:
+    print("Idade inválida!")
+elif idade  > 18:
     print("Você é de maior!")
+else:
+    print("Você é de menor!")
---
+++ prática/Exercicios/Questao2.py
@@ -0,0 +1,14 @@
+#**
+# 2 - Faça um programa que receba duas notas digitadas pelo usuário. 
+# Se a nota for maior ou igual a seis, escreva aprovado, senão escreva reprovado.    
+#*
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+
+media = (nota1 + nota2) / 2
+print("Sua média é: %.2f" %(media))
+
+if media > 7:
+    print("Aluno aprovado com média: %.2f" %(media))
+else:
+    print("Aluno reprovado com média: %.2f" %(media))
