96feeac443e86fc9cbe9992c8f271bab6b3edc69
LOPES Anderson <andlopes@europe.corp.altran.com>
2019-07-01 21:51:41 +0100

    Exercicio 4.

---
+++ prática/Exercicios/Questao4.py
@@ -0,0 +1,16 @@
+# 4 - Escreva um programa que ordene uma lista numérica com três elementos.  
+
+lista = [8,12,-34,45,-987,9087,6,5]
+
+print(lista)
+
+for i in range(len(lista)):
+    menor = i
+    for j in range(i+1,len(lista)):
+        if lista[j] < lista[menor]:
+            menor = j
+    if lista[i] != lista[menor]:
+        aux = lista[i]
+        lista[i] = lista[menor]
+        lista[menor] = aux
+print(lista)
