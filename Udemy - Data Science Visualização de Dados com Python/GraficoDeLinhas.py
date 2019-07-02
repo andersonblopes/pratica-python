1f1acb6af42134ee9110d89ccba98229f7ac5aaf
LOPES Anderson <andlopes@europe.corp.altran.com>
2019-07-02 23:28:10 +0100

    Início Data Science Visualização de Dados com Python.

---
+++ Udemy - Data Science Visualização de Dados com Python/GraficoDeLinhas.py
@@ -0,0 +1,10 @@
+# Install matplotlib
+# PS C:\Dev\Projects\python> & C:/Users/andlopes/AppData/Local/Programs/Python/Python37-32/python.exe -m pip install matplotlib
+import matplotlib.pyplot as plt
+
+#Coordenadas X e Y
+x = [1, 2]
+y = [2, 3]
+plt.plot(x, y)
+
+plt.show()
